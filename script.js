618dab99f10a2f787b8757c74b3af729c5645c27
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-10-04 09:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function updateSize(){
+	let cont = document.getElementById("cont");
+	let width = window.innerWidth|| document.documentElement.clientWidth || document.body.clientWidth;
+	let height = window.innerHeight|| document.documentElement.clientHeight || document.body.clientHeight;
+
+	cont.textContent = `Width : ${width} Height : ${height}`;
+}
+updateSize();
+window.addEventListner("resize", updateSize());
